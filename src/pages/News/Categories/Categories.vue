d9ca24d2950100949c4b20f55e9df2c42a928f71
fernand0mart1n <fernando_valde@hotmail.com>
2023-11-21 08:51:37 -0300

    fix news list

--- src/pages/News/Categories/Categories.vue
+++ src/pages/News/Categories/Categories.vue
@@ -55,6 +55,64 @@
                 </q-list>
             </div>
         </div>
+
+        <div class="row q-pa-sm">
+          <div class="col-12">
+            <p class="text-h5">Listado de noticias</p>
+          </div>
+          <div v-if="newsList.length == 0" class="col-12">
+            <q-list bordered separator>
+              <q-item>
+                <q-item-section>
+                  <q-item-label>
+                    <q-skeleton type="text" />
+                  </q-item-label>
+                  <q-item-label caption>
+                    <q-skeleton type="text" width="65%" />
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+          <div v-else class="col-12">
+            <q-list bordered separator>
+              <q-item v-for="(news, key) in newsList" :key="key">
+                <q-item-section avatar top>
+                  <q-avatar icon="feed" color="primary" text-color="white" />
+                </q-item-section>
+
+                <q-item-section>
+                  <q-item-label>
+                    <b>Titulo: </b> {{ news.titulo }} | <b>Autor: </b>
+                    {{ news.autor }}
+                  </q-item-label>
+                  <q-item-label caption lines="3">
+                    <b>Cuerpo: </b> {{ news.cuerpo }}
+                  </q-item-label>
+                </q-item-section>
+
+                <q-item-section side>
+                  <q-btn color="grey-7" round flat icon="more_vert">
+                    <q-menu cover auto-close>
+                      <q-list>
+                        <q-item clickable :to="'/news/info/' + news.id">
+                          <q-item-section>Ver mas</q-item-section>
+                        </q-item>
+                        <q-item clickable :to="'/news/edit/' + news.id">
+                          <q-item-section>Editar</q-item-section>
+                        </q-item>
+                        <q-item clickable @click="removeNews(news.id)">
+                          <q-item-section>Borrar</q-item-section>
+                        </q-item>
+                      </q-list>
+                    </q-menu>
+                  </q-btn>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </div>
+
         <q-page-sticky position="bottom-right" :offset="[18, 18]">
             <q-fab v-model="fabLeft" vertical-actions-align="right" color="green" icon="add" direction="up">
                 <q-fab-action label-position="right" color="primary" icon="add" label="Crear noticia" :to="'news/create'" />
@@ -68,6 +126,7 @@
 <script>
 import { computed } from "vue";
 import { useCategories } from "stores/categories";
+import { useNews } from "stores/news";
 import { ref } from "vue";
 import moment from "moment";
 
@@ -75,21 +134,30 @@ export default {
     name: "CategoriesSection",
     setup() {
         const store = useCategories();
+        const storeNews = useNews();
         const categoriesList = computed(() => store.getCategoriesList);
+        const newsList = computed(() => storeNews.getNewsList);
 
         return {
             fabLeft: ref(false),
             store,
+            storeNews,
             categoriesList,
+            newsList
         };
     },
     async mounted() {
         await this.store.getApiCategories();
+        await this.storeNews.getApiNews();
     },
     methods: {
         async removeCategory(section_id) {
             await this.store.deleteCategory(section_id);
             await this.store.getApiCategories();
+        },
+        async removeNews(news_id) {
+            await this.store.deleteNews(news_id);
+            await this.store.getApiNews();
         },
         formatDate(date) {
             return moment(String(date)).format("MM/DD/YYYY hh:mm");
