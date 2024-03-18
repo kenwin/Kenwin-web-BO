9215770f68a42f23c2134480d69ce497d7adf01e
Fer <fernando_valde@hotmail.com>
2024-03-18 00:53:36 -0300

    edit fix

--- src/pages/News/NewsCreate.vue
+++ src/pages/News/NewsCreate.vue
@@ -180,24 +180,6 @@ export default {
         image: ref(null),
         video: ref(null),
       },
-      editorInit: {
-        toolbar_mode: 'sliding',
-        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
-        images_upload_url: "https://api2023.kenwin.net/api/downloads/create_resource",
-        mergetags_list: [
-          { value: 'First.Name', title: 'First Name' },
-          { value: 'Email', title: 'Email' },
-        ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-        language_url: '/langs/es_MX.js',
-        language: 'es_MX',
-        menubar: "file edit view insert format tools table help",
-        toolbar_mode: "floating",
-        height: 600,
-      },
     };
   },
   mounted() {
@@ -352,7 +334,7 @@ export default {
       editor.setData(this.form.cuerpo);
     }).catch(error => {
       console.error('Error al inicializar CKEditor:', error);
-    });
+    })
   },
   methods: {
     async onSubmit() {
